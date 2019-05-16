90e023a250f4c592def90f39f3af4a691733d2fc
Danijel Milenkovic <danijelmilenkovic1108@gmail.com>
2019-05-16 22:50:35 +0800

    first test with nRF

--- BleService/www/js/index.js
+++ BleService/www/js/index.js
@@ -12,57 +12,86 @@ function stringToBytes(string) {
     return array.buffer;
 }
 
-// Nordic UART Service
-var SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
-var TX_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
-var RX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E';
+function toUTF8Array(str) {
+    var utf8 = [];
+    for (var i = 0; i < str.length; i++) {
+        var charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6),
+                0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(0xe0 | (charcode >> 12),
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f));
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode = 0x10000 + (((charcode & 0x3ff) << 10)
+                | (str.charCodeAt(i) & 0x3ff))
+            utf8.push(0xf0 | (charcode >> 18),
+                0x80 | ((charcode >> 12) & 0x3f),
+                0x80 | ((charcode >> 6) & 0x3f),
+                0x80 | (charcode & 0x3f));
+        }
+    }
+    return utf8;
+}
+
+function synch() {
+    var socket = new Socket();
+    socket.open(
+        "192.168.1.68",
+        3737,
+        function () {
+            console.log("Connected");
+            var dataString = dataS;
+
+            var data = new toUTF8Array(dataString)
+            socket.write(data);
+            console.log("Socket sent");
+            socket.shutdownWrite();
+        },
+        function (errorMessage) {
+            alert("Подключитесь к интернету");
+            location.href = 'index.html';
+            console.log("No connection");
+        });
+}
+
 
 // Nordic UART Service
 var SERVICE_UUID = 'BA10';  //PORT BA52
-var TX_UUID = '00F0';
+var TX_UUID = 'BA52';
 var RX_UUID = '00F1';
 var update = []
 
+var dataS = '';
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         blePeripheral.onWriteRequest(app.didReceiveWriteRequest);
         blePeripheral.onBluetoothStateChange(app.onBluetoothStateChange);
 
         // 2 different ways to create the service: API calls or JSON
-        //app.createService();
+        // app.createService();
         app.createServiceJSON();
 
     },
-    createService: function() {
-        // https://learn.adafruit.com/introducing-the-adafruit-bluefruit-le-uart-friend/uart-service
-        // Characteristic names are assigned from the point of view of the Central device
-
-        var property = blePeripheral.properties;
-        var permission = blePeripheral.permissions;
-
-        Promise.all([
-            blePeripheral.createService(SERVICE_UUID),
-            blePeripheral.addCharacteristic(SERVICE_UUID, TX_UUID, property.WRITE, permission.WRITEABLE),
-            blePeripheral.addCharacteristic(SERVICE_UUID, RX_UUID, property.READ | property.NOTIFY, permission.READABLE),
-            blePeripheral.publishService(SERVICE_UUID),
-            blePeripheral.startAdvertising(SERVICE_UUID, 'UART')
-        ]).then(() =>{
-            console.log ('Created UART Service');
-        });
-
-        blePeripheral.onWriteRequest(app.didReceiveWriteRequest);
-    },
+    
     createServiceJSON: function() {
-        // https://learn.adafruit.com/introducing-the-adafruit-bluefruit-le-uart-friend/uart-service
-        // Characteristic names are assigned from the point of view of the Central device
-
         var property = blePeripheral.properties;
         var permission = blePeripheral.permissions;
 
@@ -71,23 +100,12 @@ var app = {
             characteristics: [
                 {
                     uuid: TX_UUID,
-                    properties: property.WRITE,
+                    properties: property.READ | property.NOTIFY | property.WRITE,
                     permissions: permission.WRITEABLE,
                     descriptors: [
                         {
                             uuid: '2901',
                             value: 'Transmit'
-                        }
-                    ]
-                },
-                {
-                    uuid: RX_UUID,
-                    properties: property.READ | property.NOTIFY,
-                    permissions: permission.READABLE,
-                    descriptors: [
-                        {
-                            uuid: '2901',
-                            value: 'Receive'
                         }
                     ]
                 }
@@ -98,34 +116,50 @@ var app = {
             blePeripheral.createServiceFromJSON(uartService),
             blePeripheral.startAdvertising(uartService.uuid, 'UART')
         ]).then(() => {
-            console.log ('Created UART Service');
+            console.log('Created UART Service');
         });
     },
-    updateCharacteristicValue: function() {
-        var input = document.querySelector('input');
-        var bytes = stringToBytes(input.value);
+    
+    // updateCharacteristicValue: function () {
+    //     var input = document.querySelector('input');
+    //     var bytes = stringToBytes(input.value);
 
-        var success = function() {
-            outputDiv.innerHTML += messageInput.value + '<br/>';
-            console.log('Updated RX value to ' + input.value);
-        };
-        var failure = function() {
-            console.log('Error updating RX value.');
-        };
+    //     var success = function () {
+    //         console.log('Updated RX value to ' + input.value);
+    //     };
+    //     var failure = function () {
+    //         console.log('Error updating RX value.');
+    //     };
 
-        blePeripheral.setCharacteristicValue(SERVICE_UUID, RX_UUID, bytes).
-            then(success, failure);
+    //     blePeripheral.setCharacteristicValue(SERVICE_UUID, RX_UUID, bytes).
+    //         then(success, failure);
 
-    },
-    didReceiveWriteRequest: function(request) {
+    // },
+
+    didReceiveWriteRequest: function (request) {
         var message = bytesToString(request.value);
         console.log(message);
         // warning: message should be escaped to avoid javascript injection
-        outputDiv.innerHTML += '<i>' + message + '</i><br/>';
+        if (message != "0") {
+            app.stateChange(message);
+        }
     },
-    onBluetoothStateChange: function(state) {
+    onBluetoothStateChange: function (state) {
         console.log('Bluetooth State is', state);
-        outputDiv.innerHTML += 'Bluetooth  is ' +  state + '<br/>';
+    },
+
+    stateChange(tlg) {
+        document.getElementById("prohod").innerHTML = "Открыто";
+        var date = Math.round(+new Date() / 1000);
+        var message = date + "=" + tlg + "=" + localStorage.getItem("username") + "=" + localStorage.getItem("work") + "=" + localStorage.getItem("company") + "=" + localStorage.getItem("phone");
+        dataS = message;
+        synch();
+        console.log(message);
+        setTimeout(function () {
+            if (1 == 1) {
+                document.getElementById("prohod").innerHTML = "Закрыто";
+            }
+        }, 5000);
     }
 };
 
